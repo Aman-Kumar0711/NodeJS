901e0bebc8fbb4b59016b775557bda85431de9aa
Aman <amanchahar175@gmail.com>
2024-07-20 22:07:58 +0530

    crud ops in async manner

---
+++ fsAsync/crudAsync.js
@@ -0,0 +1,24 @@
+//isme main async tareeke se CRUD(Create Read Update Delete) operations karke dekhunga
+const fs = require("fs");
+// fs.mkdir("newFolder", (err) => {
+//   console.log("Folder Created");
+// });
+// fs.writeFile(
+//   "./newFolder/read.txt",
+//   "I have created a folder asynchronously where I am creating this file called read.txt ",
+//   (err) => {
+//     console.log("File has been created");
+//   }
+// );
+// fs.appendFile("./newFolder/read.txt", " and some random stuff", (err) => {
+//   console.log("changes has been made");
+// });
+// fs.readFile("./newFolder/read.txt", "UTF-8", (err, res) => {
+//   console.log(res);
+// });
+// fs.unlink("./newFolder/read.txt", (err) => {
+//   console.log("File has been deleted");
+// });
+// fs.rmdir("newFolder", (err) => {
+//   console.log("Folder has been deleted");
+// });
